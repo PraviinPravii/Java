871a451720d055861babdb4b3c946f0b880527f4
Praveen Achari <praveen.achari@in.abb.com>
2023-07-28 09:18:07 +0530

    static method and function

--- Class3/Staticmethod/A.java
+++ Class3/Staticmethod/A.java
@@ -1,4 +1,17 @@
 package Class3.Staticmethod;
 
 public class A {
+    int nst=89;
+    static  int st=992;
+    void func(int x,int y){
+        System.out.println(x+y);
+        System.out.println(this.st);
+        this.sfunc(3,4);
+    }
+    static void sfunc(int x,int y){
+        System.out.println(x+y);
+        System.out.println(st);
+
+
+    }
 }
--- Class3/Staticmethod/Client.java
+++ Class3/Staticmethod/Client.java
@@ -1,5 +1,29 @@
 package Class3.Staticmethod;
 
-public class Client {
+import java.util.Arrays;
+import java.util.HashMap;
 
+public class Client {
+    public static void main(String[] args) {
+        int h=10;
+        A obj=new A();
+        obj.sfunc(3,5);
+        obj.func(38,46);
+        A.sfunc(293,49); ///Static level call or class level call
+//        A.func(3,4);// Error object level function cant be accessed on class level
+        int[] arr={283,482,282,2,392,2983,382,24}; //Static
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        HashMap<Integer, Integer> hash=new HashMap<Integer, Integer>();
+//        for(int i=0;i<arr.length;i++){
+//            hash.put(i,arr[i]);
+//        }
+
+
+
+
+
+
+
+    }
 }
